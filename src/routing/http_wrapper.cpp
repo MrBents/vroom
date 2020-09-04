1ab3c6eb153e0afefa1ca8cf95596df8f5e780fa
Max <univbentata@hotmail.com>
2020-09-03 23:42:22 -0700

    9th commit

--- src/routing/http_wrapper.cpp
+++ src/routing/http_wrapper.cpp
@@ -91,7 +91,7 @@ std::string HttpWrapper::ssl_send_then_receive(const std::string& query) const {
 
     tcp::resolver r(io_service);
 
-    tcp::resolver::query q(_server.host, _server.port, 0);
+    tcp::resolver::query q(_server.host, _server.port, asio::ip::resolver_query_base::numeric_service);
 
     asio::connect(ssock.lowest_layer(), r.resolve(q));
     ssock.handshake(asio::ssl::stream_base::handshake_type::client);
